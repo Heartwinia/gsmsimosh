fc7cfbd241c58fe45aa5396a8608274624f81eac
Heartwin Karki <manjeetcarkey@live.com>
2018-10-01 20:21:24 +0545

    header and banner fixed

--- index.php
+++ index.php
@@ -14,9 +14,35 @@
 
 <div class="hero-content">
 	<?php include"header.php"; ?>
+
+	<div class="hero-banner">
+		<div class="banner-content">
+			<div class="hero-right-content">
+				<img src="img/mobile.png" alt="">
+			</div>
+			<!-- eo hero right content -->
+
+			<div class="hero-left-content">
+				<h2>The latest version of all mobile software</h2>
+
+				<p>If you are looking for the latest softwares, look no further. It is best place to download latest software for mobile.</p>
+
+				<a href="register.php" class="btn banner-btn">Start now</a>
+			</div>
+			<!-- eo hero left content -->
+		</div>
+	</div>
+	<!-- eo hero banner -->
+
 </div>
 
-hello
+<div class="divider"></div>
+
+<div class="container">
+<div class="items-lists">
+	<h2 class="main-header">select brand</h2>
+</div>
+</div>
 
 </body>
 
